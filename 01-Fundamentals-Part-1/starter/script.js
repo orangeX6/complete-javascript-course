6f734cea7a9e24f24caa8259206d1c4de16d8b44
pranav <naringrekarpranav6@live.com>
2021-11-28 13:46:29 +0530

    Initial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,54 @@
+/*
+let js = 'amazing';
+// if (js === 'amazing') alert('Javascript is FUN!');
+console.log(40 + 8 + 23 - 10);
+console.log('Pranav');
+let firstName = 'Sakshi';
+console.log(firstName);
+
+//In JS usually people use camelCase
+
+
+
+let marksWeight = 78;
+let marksHeight = 1.69;
+
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let mass = marksWeight;
+let height = marksHeight;
+let BMI = mass / height ** 2;
+
+console.log(BMI);
+
+mass = johnWeight;
+height = johnHeight;
+console.log(BMI);
+
+
+console.log(true);
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun)
+console.log(typeof false);
+// console.log(typeof 23.56);
+console.log(typeof 35);
+console.log(typeof 'Pranav');
+console.log(typeof `Pranav`)
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun)
+let chicken;
+console.log(chicken)
+console.log(typeof chicken);
+chicken = 'Tandoori';
+console.log(typeof chicken);
+console.log(typeof null);
+
+*/
+
+//TYPES OF VARIABLES - 
+// 1 - LET
+// 2 - CONST
+// 3 - VAR
+//  By default just alUSE let when you know you have to change the value of the variable. 
