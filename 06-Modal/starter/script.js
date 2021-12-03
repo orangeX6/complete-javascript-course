2838fd02caffd48507ebe6600c277e45968ac3d7
Pranav <naringrekarpranav6@live.com>
2021-12-03 20:04:04 +0530

    Modal Project Complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+//WE DONT HAVE TO WRITE BRACKETS FOR ONE LINE OF LOOP
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//KEYBOARD EVENTS ARE GLOBAL EVENTS, BECAUSE DO NOT HAPPEN ON ONE
+// SPECIFIC ELEMENT, AND FOR GLOBAL EVENTS LIKE KEYBOARD EVENTS, WE LISTEN ON
+// WHOLE DOCUMENTS
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
