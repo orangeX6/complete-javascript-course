e54b2a10603447cf74313afe5c4be6efee77c7c9
Pranav <naringrekarpranav6@live.com>
2022-01-10 00:31:20 +0530

    Mapty Final

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,6 +3,87 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+//////////////////////////////////////////////////
+/////////////////////////////////////////////////
+//-> WORKOUT PARENT CLASS
+class Workout {
+  //! STILL NOT A PART OF JS OFFICIALLY
+  //? In Real world, we should never create ids on our own but always use some library to create a unique id.
+  //Here we are just converting date to numeric and using last 10 digits as id
+  id = (Date.now() + '').slice(-10);
+  date = new Date();
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    //? DEFINING DATE AND ID ACC. TO ES6
+    //this.date = ...
+    //this.id = ...
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; // in kms
+    this.duration = duration; // in mins
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
+  }
+}
+
+/////////////////////////////////////////////
+/////////////////////////////////////////
+//-> RUNNING CHILD CLASS
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    // // this.type = 'running'
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    //  min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+//////////////////////////////////////////////
+/////////////////////////////////////////////
+//-> CYCLING CHILD CLASS
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([72, 19], 5.2, 24, 178);
+// const cycle1 = new Cycling([72, 19], 27, 95, 523);
+// console.log(run1, cycle1);
+
+////////////////////////////////////////
+////////////////////////////////////////
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,148 +92,274 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// 231. How to Plan a Web Project
-// 232. Using the Geolocation API
-// 233. Displaying a Map Using Leaflet Library
-// 234. Displaying a Map Marker
-// 235. Rendering Workout Input Form
-// 236. Project Architecture
-// 237. Refactoring for Project Architecture
-// 238. Managing Workout Data: Creating Classes
-// 239. Creating a New Workout
-// 240. Rendering Workouts
-// 241. Move to Marker On Click
-// 242. Working with localStorage
-// 243. Final Considerations
+//-> APPLICATION ARCHITECTURE
+class App {
+  #map;
+  #mapEvent;
+  #mapZoomLevel = 13;
+  #workouts = [];
 
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
+  //>> CONSTRUCTOR
+  constructor() {
+    //// GET USER'S POSITION
+    this._getPosition();
 
-// 231. How to Plan a Web Project
+    ////GET DATA FROM LOCAL STORAGE
+    this._getLocalStorage();
 
-//>> Stage 1 -  PROJECT PLANNING  -
-//-> 1. User Stories
-// Description of the applications functionality from the user's perspective. All user stories put together describes the entire application
-//-> 2. Features
-//-> 3. FlowChart
-// WHAT we will build
-//-> 4. Architecture
-// HOW we will build it
-//>> Stage 2 - DEVELOPMENT
-//Implementation of the code
+    //// ATTACH EVENT HANDLERS
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    //! no need to bind this, as the _toggleElev function doesn't use it
+    inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+  }
 
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
+  //>> GET POSITION
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+  }
 
-//L is leaflets namespace
-//L is a global variable in leaflets script and so we can access it from here.
-//! It is important that this script is included after the leaflets script in html
-//? So script.js has access to variables in leaflets script and other.js but leaflets and other.js script won't have access to variables in script.js
-//DECLARED IN other.js
-//console.log(firstName);
+  //>> LOAD MAP
+  _loadMap(position) {
+    //// console.log(this);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    //// console.log(position);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
 
-//>>setView method of leaflet -
-//setView([latitude,longitude], zoomLevel)
-//>>TitleLayer ->
-//titleLayer specifies which map you want to use- openstreet, googlemaps,etc. and the themes(appearance)
-//default - 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-//>> Documentation -
-//-> https://leafletjs.com/reference.html
+    const coords = [latitude, longitude];
 
-let map, mapEvent;
+    //('map') is the id of html element
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+    //// console.log(this.#map);
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      // console.log(position);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-      const coords = [latitude, longitude];
+    //Handling Clicks on Map
+    this.#map.on('click', this._showForm.bind(this));
 
-      //('map') is the id of html element
-      map = L.map('map').setView(coords, 13);
-      console.log(map);
+    //Render the markers
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
+  }
 
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  //>> DISPLAY FORM
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-      //Handling Clicks on Map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('Could not get your position');
+  //>> HIDE FORM
+  _hideForm() {
+    // // EMPTY INPUTS
+    // prettier-ignore
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
+  //>> TOGGLE ELEVATION FIELD
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  //>> CREATE NEW WORKOUT
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Get data from the form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    // //console.log(this.#mapEvent);
+    let workout;
+
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    // If activity running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //// Check if data is valid
+      if (
+        // // !Number.isFinite(distance) ||
+        // // !Number.isFinite(duration) ||
+        // // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to positive numbers');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
     }
-  );
+
+    // If activity cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      //// Check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to positive numbers');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    // Add new object to workout array
+    this.#workouts.push(workout);
+    // //console.log(workout);
+
+    // Render workout on map as marker
+    //? No need to use .bind(this) because we ourselves are calling this method using this
+    this._renderWorkoutMarker(workout);
+
+    // Render workout on list
+    this._renderWorkout(workout);
+
+    // Hide form + Clear Input Fields
+    this._hideForm();
+
+    //Set local storage to all workouts
+    this._setLocalStorage();
+  }
+
+  //>> DISPLAY WORKOUT TAGS ON MAP.
+  //CALLED FROM _newWorkout
+  _renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+        //*  .setContent('Workout')  //Also works
+      )
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♂️'} ${workout.description}`
+      )
+      .openPopup();
+  }
+
+  //>> DISPLAY WORKOUT LIST
+  //CALLED FROM _newWorkout
+  _renderWorkout(workout) {
+    let html = ` 
+        <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon"> ${
+            workout.type === 'running' ? '🏃‍♂️' : '🚴‍♂️'
+          }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+    `;
+
+    if (workout.type === 'running')
+      html += `
+          <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.pace.toFixed(1)}</span>
+          <span class="workout__unit">min/km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">🦶🏼</span>
+          <span class="workout__value">${workout.cadence}</span>
+          <span class="workout__unit">spm</span>
+        </div>
+      </li>
+        `;
+
+    if (workout.type === 'cycling')
+      html += `
+          <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.speed.toFixed(1)}</span>
+          <span class="workout__unit">km/h</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⛰</span>
+          <span class="workout__value">${workout.elevationGain}</span>
+          <span class="workout__unit">m</span>
+        </div>
+      </li> 
+     `;
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  //>> Move to Popup on Map
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    //// console.log(workoutEl, this);
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    // //console.log(workout);
+
+    // Using the public interface
+    //!THis won't work when the objects are retrieved from local storage. The reason being, the prototype chain is lost.
+    //-> CHECK THE PROTOTYPES ON OBJECTS RETRIEVED FROM LOCAL STORAGE VS NEW OBJECTS CREATED
+    //// workout.click();
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  }
+
+  //>> STORING DATA IN BROWSER(LOCAL STORAGE)
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  //>> DISPLAYING DATA FROM LOCAL STORAGE IN THE APP
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    //// console.log(data);
+    if (!data) return;
+
+    this.#workouts = data;
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload(); //reloads the page in browser
+  }
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  //Clear Input Fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  //Display Marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-      // .setContent('Workout')
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,86 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//// 246. Asynchronous JavaScript, AJAX and APIs
+//// 248. Our First AJAX Call: XMLHttpRequest
+//// 249. [OPTIONAL] How the Web Works: Requests and Responses
+//// 250. Welcome to Callback Hell
+//// 251. Promises and the Fetch API
+//// 252. Consuming Promises
+//// 253. Chaining Promises
+//// 254. Handling Rejected Promises
+//// 255. Throwing Errors Manually
+//// 256. Coding Challenge #1
+//// 257. Asynchronous Behind the Scenes: The Event Loop
+//// 258. The Event Loop in Practice
+//// 259. Building a Simple Promise
+//// 260. Promisifying the Geolocation API
+//// 261. Coding Challenge #2
+//// 262. Consuming Promises with Async/Await
+//// 263. Error Handling With try...catch
+//// 264. Returning Values from Async Functions
+//// 265. Running Promises in Parallel
+//// 266. Other Promise Combinators: race, allSettled and any
+//// 267. Coding Challenge #3
+
+// 246. Asynchronous JavaScript, AJAX and APIs
+//>> Coordinating behavior of a program over a period of time
+//>> Asynchronous means not occurring at the same time
+//-> Asynchronous code is executed after a task that runs in the "background" finishes.
+//-> Asynchronous code is  'non-blocking'
+//-> Execution doesn't wait for an asynchronous task to finish its work.
+
+//Example of asynchronous code in js
+//* 1. Asynchronous image loading with event and callback
+//img.src -> loading of image happens asynchronously
+//we provide the callback function to the load event to call the function once the image is loaded and not right away
+//* 2. Geolocation API or AJAX calls, etc
+//? Callback functions alone do NOT make code asynchronous
+
+//////////////////////////////////////////////////
+//-> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//->> AJAX
+//>> Stands for Asynchronous Javascript And XML.
+//-> Allows us to communicate with remote web servers in an asynchronous way.
+//-> With AJAX calls, we can REQUEST DATA from web servers dynamically
+
+//?HOW AJAX WORKS ?
+//? Client <====Request/Response====> Webserver
+
+//? AJAX is a term which got popular back and so we still use it. But most apis don't use XML data format these days
+//-> Most popular API data format now is JSON
+
+//////////////////////////////////////////////////
+//-> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//->> API
+//>> Stands for Application Programming Interface.
+//-> A piece of software that can be used by another piece of software, in order to allow applications to talk to each other
+//Example - DOM API, Geolocation API, etc.
+
+//? "Online" API - Application running on a server, that receives requests for data, and sends data back to response.
+//? We simply call these APIs or WebAPIs
+
+//There are APIs about everything. Example ->
+// For travelling app you can get an api for
+//* Weather data
+//* Data about countries
+//* Flights Data
+//* Currency conversion data
+//* APIs for sending email or SMS
+//* Google Maps
+//There are millions of possibilities
+
+// 247. IMPORTANT: API URL Change
+//-> USE THIS
+// https://restcountries.com/v2/
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+// 248. Our First AJAX Call: XMLHttpRequest
+//?There are multiple ways to do AJAX calls
+//>> We will use XMLHttpRequest in this lecture
+//CORS - Cross Origin Resource Sharing
+const request = new XMLHttpRequest();
+
+request.open('GET', '');
