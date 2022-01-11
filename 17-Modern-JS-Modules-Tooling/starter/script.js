a0d050ec1ca82f92228bb844a85501316174a35b
Pranav <naringrekarpranav6@live.com>
2022-01-11 20:09:36 +0530

    Tools

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 270. An Overview of Modern JavaScript Development
+// 271. An Overview of Modules in JavaScript
+// 272. Exporting and Importing in ES6 Modules
+// 273. Top-Level await (ES2022)
+// 274. The Module Pattern
+// 275. CommonJS Modules
+// 276. A Brief Introduction to the Command Line
+// 277. Introduction to NPM
+// 278. Bundling With Parcel and NPM Scripts
+// 279. Configuring Babel and Polyfilling
+// 280. Review: Writing Clean and Modern JavaScript
+// 281. Let's Fix Some Bad Code: Part 1
+// 282. Declarative and Functional JavaScript Principles
+// 283. Let's Fix Some Bad Code: Part 2
